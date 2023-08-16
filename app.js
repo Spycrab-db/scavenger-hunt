487e75c4d6d4ac1a7bc7c3f59e2bb39b278b20e0
Gavin Yan <120120964+yangavin@users.noreply.github.com>
2023-08-16 17:59:07 -0400

    Create route for cipher questions and final submission

--- app.js
+++ app.js
@@ -26,19 +26,26 @@ app.get("/", (req, res) => {
 
 app.get("/:id", async (req, res) => {
   const puzzle = await Puzzle.findById(req.params.id);
-  if (puzzle) {
+  if (!puzzle) return res.status(404).send("Not Found");
+  if (Number(puzzle.number) <= 5) {
     return res.render(puzzle.number, {
       id: puzzle.id,
       title: puzzle.title,
       question: puzzle.question,
     });
   }
-  return res.status(404).send("Not Found");
+  return res.render("cipher", {
+    id: puzzle.id,
+    title: puzzle.title,
+    question: puzzle.question,
+    code: puzzle.code,
+  });
 });
 
 app.post("/:id", async (req, res) => {
   const puzzle = await Puzzle.findById(req.params.id);
   if (puzzle) {
+    // Check answer as an array
     if (req.body.checkList) {
       const answer = [];
       const keys = Object.keys(req.body);
@@ -50,8 +57,13 @@ app.post("/:id", async (req, res) => {
       }
       return res.redirect(`/${req.params.id}`);
     }
+
+    // Check answer against an array of correct answers
     if (!Array.isArray(puzzle.answer)) puzzle.answer = [puzzle.answer];
     if (puzzle.answer.includes(req.body.answer.toLowerCase())) {
+      if (puzzle.number === "8") {
+        return res.render("win");
+      }
       return res.render("reward", { reward: puzzle.reward });
     }
     return res.redirect(`/${req.params.id}`);
