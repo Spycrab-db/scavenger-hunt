8f6c6289a17df1ba228025fa07467714aaa02a91
Gavin Yan <120120964+yangavin@users.noreply.github.com>
2023-08-16 14:29:01 -0400

    Add logic for list answer

--- app.js
+++ app.js
@@ -8,9 +8,14 @@ app.set("view engine", "ejs");
 const port = process.env.PORT || 3000;
 const dbURL = process.env.DB_URL || "mongodb://127.0.0.1:27017/scavenger-hunt";
 
-mongoose.connect(dbURL).then(() => {
-  console.log("CONNECTED TO DATABASE");
-});
+mongoose
+  .connect(dbURL)
+  .then(() => {
+    console.log("CONNECTED TO DATABASE");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 app.use(express.urlencoded({ extended: false }));
 
@@ -22,6 +27,7 @@ app.get("/:id", async (req, res) => {
   const puzzle = await Puzzle.findById(req.params.id);
   if (puzzle) {
     return res.render(puzzle.number, {
+      id: puzzle.id,
       title: puzzle.title,
       question: puzzle.question,
     });
@@ -32,10 +38,22 @@ app.get("/:id", async (req, res) => {
 app.post("/:id", async (req, res) => {
   const puzzle = await Puzzle.findById(req.params.id);
   if (puzzle) {
+    if (req.body.checkList) {
+      const answer = [];
+      const keys = Object.keys(req.body);
+      keys.forEach((key) => {
+        if (key !== "checkList") answer.push(req.body[key]);
+      });
+      if (JSON.stringify(answer) === JSON.stringify(puzzle.answer)) {
+        return res.render("reward", { reward: puzzle.reward });
+      }
+      return res.redirect(`/${req.params.id}`);
+    }
     if (!Array.isArray(puzzle.answer)) puzzle.answer = [puzzle.answer];
     if (puzzle.answer.includes(req.body.answer.toLowerCase())) {
-      return res.send(puzzle.reward);
+      return res.render("reward", { reward: puzzle.reward });
     }
+    return res.redirect(`/${req.params.id}`);
   }
   return res.redirect(`/${req.params.id}`);
 });
